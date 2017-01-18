6473f0146d72eaecd4f0756b457ca4523e80abda
ASUS <ASUS@DESKTOP-80CR18K>
2017-01-18 11:31:39 +0100

    Premier commit

---
+++ src/main/java/web4si/Web4siApplication.java
@@ -0,0 +1,12 @@
+package web4si;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web4siApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web4siApplication.class, args);
+	}
+}
