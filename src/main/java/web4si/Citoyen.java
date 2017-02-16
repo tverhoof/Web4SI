1862cf855c266638e9ce917da29995b7d718b39c
aazri <aazri@10.10.169.32>
2017-02-16 11:53:40 +0100

    debut Citoyen

--- src/main/java/web4si/Citoyen.java
+++ src/main/java/web4si/Citoyen.java
@@ -5,61 +5,19 @@
  */
 package web4si;
 
+import java.util.ArrayList;
+
+
 /**
  *
  * @author aazri
  */
-public class Citoyen 
-{
-    
-    // Attributs
-    
-    
-    private int idCitoyen;
-    private String nomCitoyen;
-    private String prenomCitoyen;
-    private String adresseCitoyen;
-    
-    
-    // Propriétés
-    
-    
-    public int getIdCitoyen() {
-        return this.idCitoyen;
-    }
-
-    public void setIdCitoyen(int idCitoyen) {
-        this.idCitoyen = idCitoyen;
-    }
-
-    public String getNomCitoyen() {
-        return this.nomCitoyen;
-    }
-
-    public void setNomCitoyen(String nomCitoyen) {
-        this.nomCitoyen = nomCitoyen;
-    }
-
-    public String getPrenomCitoyen() {
-        return this.prenomCitoyen;
-    }
-
-    public void setPrenomCitoyen(String prenomCitoyen) {
-        this.prenomCitoyen = prenomCitoyen;
-    }
-
-    public String getAdresseCitoyen() {
-        return this.adresseCitoyen;
-    }
-
-    public void setAdresseCitoyen(String adresseCitoyen) {
-        this.adresseCitoyen = adresseCitoyen;
-    }
-    
-    
-    // Constructeurs
-    
-    public Citoyen() {} // JPA !!
+public class Citoyen {
+    int idCitoyen;
+    String nomCitoyen;
+    String prenomCitoyen;
+    String adresseCitoyen;
+    String objetIncident;
     
 
     public Citoyen(int idCitoyen, String nomCitoyen, String prenomCitoyen, String adresseCitoyen) {
@@ -67,5 +25,59 @@ public class Citoyen
         this.nomCitoyen = nomCitoyen;
         this.prenomCitoyen = prenomCitoyen;
         this.adresseCitoyen = adresseCitoyen;
-    }    
+    }
+
+    public int getIdCitoyen() {
+        return idCitoyen;
+    }
+
+    public void setIdCitoyen(int idCitoyen) {
+        this.idCitoyen = idCitoyen;
+    }
+
+    public String getNomCitoyen() {
+        return nomCitoyen;
+    }
+
+    public void setNomCitoyen(String nomCitoyen) {
+        this.nomCitoyen = nomCitoyen;
+    }
+
+    public String getPrenomCitoyen() {
+        return prenomCitoyen;
+    }
+
+    public void setPrenomCitoyen(String prenomCitoyen) {
+        this.prenomCitoyen = prenomCitoyen;
+    }
+
+    public String getAdresseCitoyen() {
+        return adresseCitoyen;
+    }
+
+    public void setAdresseCitoyen(String adresseCitoyen) {
+        this.adresseCitoyen = adresseCitoyen;
+    }
+    
+    
+    public String getObjetIncident() {
+        return objetIncident;
+    }
+
+    public void setobjetIncident(String objetIncident) {
+        this.objetIncident = objetIncident;
+    }
+
+    public void reportIncident (int idCitoyen, String nomCitoyen, String prenomCitoyen, String adresseCitoyen, String objetIncident) {
+        ArrayList infoReportIncident = new ArrayList();
+        String etatDemande = "Debut"; // provisoire
+                
+        infoReportIncident.add(this.idCitoyen);
+        infoReportIncident.add(this.nomCitoyen);
+        infoReportIncident.add(this.prenomCitoyen);
+        infoReportIncident.add(this.adresseCitoyen);
+        infoReportIncident.add(etatDemande);
+        infoReportIncident.add(this.objetIncident);
+    }
+    
 }
